e88399efc526ee973829ff16af5757e9a5e069f6
Lynix <lunarlynix@gmail.com>
2022-01-31 17:45:13 -0400

    Make Vendor Image Compile.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,3 +25,11 @@ TARGET_OTA_ASSERT_DEVICE := starqlte
 
 # Kernel
 TARGET_KERNEL_CONFIG := lynx_starqlte_defconfig
+
+# Vendor init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_starqlte
+TARGET_RECOVERY_DEVICE_MODULES := libinit_starqlte
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
